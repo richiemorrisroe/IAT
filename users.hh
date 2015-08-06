4c416f5ba631c636d5e941941f20c9951fd8983c
Richie Morrisroe <morrisroe@fb.com>
2015-08-06 19:57:32 +0100

    fixed annoying subclass bug
    Apparently you need to return even optional parameters when using
    parent::__construct in Hack. I kinda get where they are coming from, but
    it would have been nice if it was called out in the docs

--- users.hh
+++ users.hh
@@ -7,11 +7,14 @@ class Person {
     this->$email = $email;
   }
 }
-// class Researcher extends Person {
-//   public  function __construct(int $rid,
-//                                ?Vector<int> $tests_developed ) {
-//     parent::__construct($rid, $email);
-//   }
-// }
-/* $new_person = new Person(1, "somebody@foobar.com"); */
-/* var_dump($new_person); */
+class Researcher extends Person {
+  public  function __construct(int $uid,
+                               string $email,
+                               ?Vector<int> $tests_taken,
+                               int $rid,
+                               ?Vector<int> $tests_developed ) {
+    parent::__construct($uid, $email, $tests_taken);
+  }
+}
+$new_person = new Person(1, "somebody@foobar.com");
+var_dump($new_person);
