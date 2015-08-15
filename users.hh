ad8b5ae982b13a6421766da45adcc7e23c6e05b8
Richie Morrisroe <richie.morrisroe@gmail.com>
2015-08-15 19:01:18 +0100

    arsed around unproductively with strict mode

--- users.hh
+++ users.hh
@@ -1,9 +1,12 @@
 <?hh //strict
 class Person {
+  public int $uid = -1;
+  private string $email = 'somebody@foobar.com';
+  public ?Vector<int> $tests_taken;
   public function __construct(int $uid,
                               string $email,
                               ?Vector<int> $tests_taken) {
-    $this->uid = $uid;
+    $this->$uid = $uid;
     $this->$email = $email;
     if($tests_taken) {
       $this->$tests_taken = $tests_taken;
@@ -16,11 +19,12 @@ class Researcher extends Person {
                                ?Vector<int> $tests_taken,
                                int $rid,
                                ?Vector<int> $tests_developed ) {
-    parent::__construct($uid, $email, $tests_taken);
+    parent::__construct($uid, $email, NULL);
   }
 }
-$new_person = new Person(1, "somebody@foobar.com", NULL);
+$new_person = new Person(1, "someperson@foobar.com", NULL);
 var_dump($new_person);
-$new_researcher = new Researcher(1, "somebody@foobar.com", NULL, 4, NULL);
+$new_researcher = new Researcher(1, "someresearcher@foobar.com", NULL, 4, NULL);
 var_dump($new_researcher);
+
   
